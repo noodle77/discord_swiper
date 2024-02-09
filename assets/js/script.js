946597aa58524fb238d121e9be5963b5387574cd
bcgosselin <bcgosselin@gmail.com>
2024-02-08 22:03:30 -0600

    updated index

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,18 @@
-//add id is 440 and is equal to temfortress news. this api will pull 440 news.
-
-// first step 
-
-
-
-// //
-
-// fetch('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
-// $(document).ready(function() {
-    //     $('#sButton').click(function() {
-        //         var gameName = $('#gameName').val();
-        
-    
-        
-        // })
-        
-const getAppId = document.querySelector('#sButton')  
-var getGameName = document.querySelector('#gameName').value;
-        
-function pullAppId () {
-    var textToAppId = new XMLHttpRequest();
-            
-    request.open("GET", "https://api.steampowered.com/ISteamApps/GetAppList/v2/");
-            
-    request.setAppId("appid", getGameName)
-    console.log("appId");
-    
-}
-            
-
-
-
-
-function pushAppId () {
-    var idToNews = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=" + appId + "&count=3&maxlength=300&format=json"
-
-fetch(idToNews)
-.then( res => res.json())
-.then(function(data) {
-    console.log(data)
+document.getElementById('sButton').addEventListener('click', async function(event) {
+    event.preventDefault();
+    await fetchData();
 });
+
+async function fetchData() {
+    let pokeName = document.getElementById('pokeData').value;
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + pokeName);
+    const data = await response.json();
+    let pokeData = data
+
+    displayData(pokeData);
 }
 
-getAppId.addEventListener("click", pushAppId);
+function displayData(pokeData) {
+    document.getElementById('newWeight').innerText = "Weight: " + pokeData.weight;
+
+}
