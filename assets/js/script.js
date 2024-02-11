15ab78b1557e607469f8c1b24765389eeb8dc98d
wacwestley30 <westleycervantes@gmail.com>
2024-02-11 08:40:40 -0500

    Added modal and a National Dex Number. Still need to add error handling and as group update README and finally add comments throughout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,27 @@
 let pokeName;
 
-var typeOne = $('#typeOne'),
+var noEvos = $('#noEvos'),
+    typeOne = $('#typeOne'),
     typeTwo = $('#typeTwo'),
     pokeCard = $('#pokeCard'),
     pokeInput = $('#pokeInput'),
+    headerOne = $('#headerOne'),
+    headerTwo = $('#headerTwo'),
+    evolvesTo = $('#evolvesTo'),
+    evoCardTo = $('#evoCardTo'),
     pokeHeight = $('#pokeHeight'),
     pokeWeight = $('#pokeWeight'),
     searchForm = $('#searchForm'),
     abilityOne = $('#abilityOne'),
     abilityTwo = $('#abilityTwo'),
+    evoCardFrom = $('#evoCardFrom'),
+    evolvesFrom = $('#evolvesFrom'),
+    headerThree = $('#headerThree'),
+    modalButton = $('#modalButton'),
     hiddenAbility = $('#hiddenAbility'),
     shinyFrontImg = $('#shinyFrontImg'),
-    defaultFrontImg = $('#defaultFrontImg');
+    defaultFrontImg = $('#defaultFrontImg'),
+    nationalDexNumber = $('#nationalDexNumber');
 
 function searchSubmitHandler(event) {
     event.preventDefault();
@@ -31,37 +41,46 @@ function getPokeApi(pokeName) {
     fetch(requestPokeApiUrl)
         .then(function (response) {
             response.json().then(function(data) {
-                console.log(data);
+                // console.log(data);
                 displayPokeStats(data);
             })
-        })
+        }
+    )
     
 }
 
 function getPokeTCGApi(pokeName) {
-    var requestPokeTCGUrl = 'https://api.pokemontcg.io/v2/cards/?q=name:' + pokeName + ' (subtypes:"basic" OR subtypes:"Stage 2") -subtypes:"TAG TEAM"';
+    var requestPokeTCGUrl = 'https://api.pokemontcg.io/v2/cards/?q=name:' + pokeName + ' -subtypes:"TAG TEAM"';
 
     fetch(requestPokeTCGUrl)
         .then(function (response) {
             response.json().then(function(results) {
-                console.log(results);
+                // console.log(results);
                 displayPokeTCGApi(results);
+                displayEvos(results);
             })
-        })
+        }
+    )
 }
 
 function displayPokeTCGApi(results) {
     pokeCard.attr('src', results.data[0].images.large)
+    if (!results.data[0].nationalPokedexNumbers[0]){
+        nationalDexNumber.text('???')
+    } else {
+        nationalDexNumber.text(results.data[0].nationalPokedexNumbers[0])
+    }
 }
 
 function displayPokeStats(data) {
-
 
     typeOne
         .text(data.types[0].type.name.toUpperCase())
         .removeClass()
         .addClass(data.types[0].type.name);
-    if (data.types[1]) {
+    if (!data.types[1]){
+        typeTwo.text('').removeClass();
+    } else {
         typeTwo
             .text(data.types[1].type.name.toUpperCase())
             .removeClass()
@@ -79,10 +98,24 @@ function displayPokeStats(data) {
     pokeHeight.text(newHeight);
     pokeWeight.text(newWeight);
 
-    abilityOne.text(data.abilities[0].ability.name)
-    if (data.abilities[1] && data.abilities[1].is_hidden === true){
+    if(data.abilities[0].is_hidden === true){
+        hiddenAbility.text(data.abilities[0].ability.name)
+    } else {
+        abilityOne.text(data.abilities[0].ability.name)
+    }
+
+    // First check for ability 1 and 2 if not there text reset.
+    // Second check for just ability 1 if not there reset text.
+    // Third check for both ability 1 and if ability 1 is_hidden
+    // Fourth check if ability 1 is not hidden
+    if (!data.abilities[1] && !data.abilities[2]) {
+        abilityTwo.text('')
+        hiddenAbility.text('')
+    } else if (!data.abilities[1]) {
+        abilityTwo.text('')
+    } else if (data.abilities[1] && data.abilities[1].is_hidden === true){
         hiddenAbility.text(data.abilities[1].ability.name)
-    } else if(data.abilities[1]){
+    } else if (data.abilities[1]){
         abilityTwo.text(data.abilities[1].ability.name)
     }
     if (data.abilities[2]){
@@ -91,6 +124,93 @@ function displayPokeStats(data) {
 
 }
 
+function displayEvos(results) {
+    noEvos.hide()
+    evolvesFrom.hide()
+    evolvesTo.hide()
+
+    if (results.data[0].evolvesFrom) {
+        let evolvesFromPokeName = results.data[0].evolvesFrom;
+        let evolvesFromUrl = 'https://api.pokemontcg.io/v2/cards/?q=name:' + evolvesFromPokeName + ' -subtypes:"TAG TEAM"';
+        fetch(evolvesFromUrl)
+            .then(function (response) {
+                response.json().then(function(fromData) {
+                    console.log(fromData);
+                    evoCardFrom.attr('src', fromData.data[0].images.small);
+                })
+            }
+        )
+    }
+
+    if (results.data[0].evolvesTo) {
+        let evolvesToPokeName = results.data[0].evolvesTo;
+        let evolvesToUrl = 'https://api.pokemontcg.io/v2/cards/?q=name:' + evolvesToPokeName + ' -subtypes:"TAG TEAM"';
+        fetch(evolvesToUrl)
+            .then(function (response) {
+                response.json().then(function(toData) {
+                    console.log(toData);
+                    evoCardTo.attr('src', toData.data[0].images.small);
+                })
+            }
+        )
+    }
+
+    if (!results.data[0].evolvesFrom && !results.data[0].evolvesTo){
+        noEvos.show()
+    } else if (results.data[0].evolvesFrom && !results.data[0].evolvesTo) {
+        evolvesFrom.show();
+    } else if (!results.data[0].evolvesFrom && results.data[0].evolvesTo) {
+        evolvesTo.show();
+    } else {
+        evolvesFrom.show();
+        evolvesTo.show();
+    }
+}
+
+// Specific to Bulma on how modals function
+function openModal() {
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+    
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+    
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+    
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+    
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+    
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+    
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+    
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        if(event.key === "Escape") {
+            closeAllModals();
+        }
+    });
+}
+
+modalButton.on('click', openModal);
 searchForm.on('submit', searchSubmitHandler);
 
 $(document).ready(() => {
